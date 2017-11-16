780b769c7189e2a5a9895e3ef7e049c4eb92947b
Alexis Tual <alexist@jfrog.com>
2017-11-16 15:02:45 +0100

    File changed by admin

---
+++ reposcript.groovy
@@ -0,0 +1,29 @@
+repoName = userInput (
+    name : "User Friendly Name",
+    type : "STRING",
+    value : "devops-repo-multi-branch",
+    description : "please provide a value"
+  )
+
+
+artifactory('MyLocal') {
+  localRepository(repoName + "-local") {
+    description "Public description"
+    notes "Some internal notes"
+    repoLayoutRef "maven-2-default"
+    archiveBrowsingEnabled false
+    packageType "maven"
+    snapshotVersionBehavior "unique"
+  }
+
+  virtualRepository(repoName + "-virtual") {
+    description "Public description"
+    notes "Some internal notes"
+    repoLayoutRef "maven-2-default"
+    repositories (["libs-releases-local", repoName + "-local", "platform-dependencies", "third-party-snapshots-local"])
+    artifactoryRequestsCanRetrieveRemoteArtifacts false
+    defaultDeploymentRepo ""
+    packageType "maven"
+  }
+  
+}
