ca890c211bc89880647d208cbab5bf2583e2f39e
Alexis Tual <alexist@jfrog.com>
2017-11-24 09:22:33 +0100

    File changed by admin

--- anotherscript.groovy
+++ anotherscript.groovy
@@ -1,2 +1,12 @@
+variable = userInput (
+    name : "User Friendly Name", // Optional
+    type : "STRING", // "BOOLEAN", "INTEGER", "SERVICE", "XRAY", "ARTIFACTORY", "REPOSITORY", "PACKAGE_TYPE"
+    //value : "default value",
+    description : "please provide a value"
+  )
+
+
+
 artifactory() {
+ 
 }
