fff76c63e90e9b0bb56f544dec870dc2e93f4e49
Alexis Tual <alexist@jfrog.com>
2017-12-19 11:16:22 +0100

    File changed by admin

--- somescript.groovy
+++ somescript.groovy
@@ -1,2 +1,21 @@
-bla
-bli
+artifactory('Art1') {
+  security {
+    
+  permissions {
+    permission('name') {
+      includesPattern '**'
+      excludesPattern ''
+      anyLocal false
+      anyRemote false
+      anyDistribution false
+      repositories (["npm-local", "docker-local"])
+      groups {
+        "itz"(['manage', 'delete', 'deploy', 'annotate', 'read'])
+      }
+    }
+  }
+
+}
+}
+
+
