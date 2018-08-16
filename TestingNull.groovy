0d9bfad8dce3f983070da7b3d099fac0cb66402f
Alexis Tual <alexist@jfrog.com>
2018-08-16 22:03:25 +0200

    File changed by admin

---
+++ TestingNull.groovy
@@ -0,0 +1,7 @@
+variable = userInput (
+    name : "User Friendly Name", // Optional
+    type : "STRING", // "BOOLEAN", "INTEGER", "SERVICE", "XRAY", "EDGE", "ARTIFACTORY", "REPOSITORY", "PACKAGE_TYPE"
+    //value : "default value",
+    description : "please provide a value"
+  )
+
