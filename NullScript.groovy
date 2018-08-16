54bc270af2c35aca0fe4b7f216c237af05f990f4
Alexis Tual <alexist@jfrog.com>
2018-08-16 11:56:11 +0200

    File changed by admin

--- NullScript.groovy
+++ NullScript.groovy
@@ -1,7 +1,7 @@
 variable = userInput (
     name : "User Friendly Name", // Optional
     type : "STRING", // "BOOLEAN", "INTEGER", "SERVICE", "XRAY", "EDGE", "ARTIFACTORY", "REPOSITORY", "PACKAGE_TYPE"
-    value : "default valueo",
+    value : "",
     description : "please provide a value"
   
   
