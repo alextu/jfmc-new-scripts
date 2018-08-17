0fb0c4aa5ce3ad857dd2dbd629f8eb4d3304d87a
Alexis Tual <alexist@jfrog.com>
2018-08-17 17:28:57 +0200

    File changed by admin

---
+++ Multi.groovy
@@ -0,0 +1,7 @@
+artis = userInput (
+    name : "Multi", // Optional
+    type : "ARTIFACTORY", // "BOOLEAN", "INTEGER", "SERVICE", "XRAY", "EDGE", "ARTIFACTORY", "REPOSITORY", "PACKAGE_TYPE"
+    //value : "default value",
+    description : "please provide a value",
+    multivalued: true
+  )
