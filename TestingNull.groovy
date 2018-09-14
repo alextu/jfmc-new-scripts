9bb74f83d02aace0f57de7bab97be2ad9a21b53c
Alexis Tual <alexist@jfrog.com>
2018-09-14 09:44:46 +0200

    File changed by admin

--- TestingNull.groovy
+++ TestingNull.groovy
@@ -1,7 +1,33 @@
-variable = userInput (
-    name : "User Friendly Name", // Optional
-    type : "ARTIFACTORY", // "BOOLEAN", "INTEGER", "SERVICE", "XRAY", "EDGE", "ARTIFACTORY", "REPOSITORY", "PACKAGE_TYPE"
+additionnalRepos = userInput (
+    name : "Repo", // Optional
+    type : "REPOSITORY", // "BOOLEAN", "INTEGER", "SERVICE", "XRAY", "EDGE", "ARTIFACTORY", "REPOSITORY", "PACKAGE_TYPE"
     //value : "default value",
+    multivalued : true,
     description : "please provide a value"
   )
+
+artifactory('Main600') {
+security {
+  permissions {
+    permission('some-perm') {
+      includesPattern '**'
+      excludesPattern ''
+      anyLocal false
+      anyRemote false
+      anyDistribution false
+      
+      repositories (["chef-local", "cran-local"] + additionnalRepos*.name) // values (["local-rep1", "local-rep2", ...]) are examples. Please set existing values from the instance 
+      users {
+        
+        'userA' (['delete', 'manage', 'annotate', 'read', 'deploy']) // value userA - is example. Please set existing user names from the instance 
+      }
+      groups {
+        'groupsG1' (['delete', 'manage', 'annotate', 'read', 'deploy']) // value groupsG1 - is example. Please set existing group names from the instance 
+      }
+    }
+  }
+
+}  
+}
+
 
