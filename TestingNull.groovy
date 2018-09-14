1a442899e5888b5f3536dcd9b622106d7ef3b794
Alexis Tual <alexist@jfrog.com>
2018-09-14 10:13:43 +0200

    File changed by admin

--- TestingNull.groovy
+++ TestingNull.groovy
@@ -1,13 +1,22 @@
 additionnalRepos = userInput (
-    name : "Repo", // Optional
-    type : "REPOSITORY", // "BOOLEAN", "INTEGER", "SERVICE", "XRAY", "EDGE", "ARTIFACTORY", "REPOSITORY", "PACKAGE_TYPE"
-    //value : "default value",
+    name : "Repos",
+    type : "REPOSITORY", 
     multivalued : true,
     description : "please provide a value"
   )
 
+variable = userInput (
+    name : "User Friendly Name", // Optional
+    type : "STRING", // "BOOLEAN", "INTEGER", "SERVICE", "XRAY", "EDGE", "ARTIFACTORY", "REPOSITORY", "PACKAGE_TYPE"
+    //value : "default value",
+    description : "please provide a value"
+  )
+
+
+
 artifactory('Main600') {
 security {
+  
   permissions {
     permission('some-perm') {
       includesPattern '**'
@@ -15,16 +24,12 @@ security {
       anyLocal false
       anyRemote false
       anyDistribution false
-      
       repositories (["chef-local", "cran-local"] + additionnalRepos*.key) // values (["local-rep1", "local-rep2", ...]) are examples. Please set existing values from the instance 
-      users {
-        
+      users { 
         'sometestuser' (['delete', 'manage', 'annotate', 'read', 'deploy']) // value userA - is example. Please set existing user names from the instance 
       }
-    
     }
   }
-
 }  
 }
 
